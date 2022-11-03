50fd8767d9de19b42cf940e0f9b960b19a47c058
saadpluton1 <saad@pluton.ltd>
2022-11-03 16:30:12 +0500

    updated

--- controllers/PlaylistController/GetOnePlaylist.js
+++ controllers/PlaylistController/GetOnePlaylist.js
@@ -8,39 +8,12 @@ import asyncHandler from "#middlewares/asyncHandler";
 
 export const getOnePlaylist = asyncHandler(async (req, res) => {
 
-  //const playlists = await Playlist.findById(req.params.id).populate('trackId');
-  
-//  const playlists = await Playlist.findById(req.params.id).populate('trackId');
+  const playlists = await Playlist.findById(req.params.id);
 
-  const result = await Playlist.aggregate([
-    {
-      $lookup:
-      {
-        from: 'songs',
-        localField: 'trackId',
-        foreignField: '_id',
-        as: 'TrackInfo'
-      }
-    },
-    {
-      $unwind: '$TrackInfo'
-    }
-
-  ]);
-console.log(result)
-  if (result.length > 0) {
-    res.status(200).json(result);
+  if (playlists) {
+    return res.status(200).json(playlists);
   }
   else {
-    res.status(404).json({ status: false, message: `No record found` });
-
+    res.status(404).json({ status: false, message: "No record found" });
   }
-
-
-  // if (playlists) {
-  //   return res.status(200).json(playlists);
-  // }
-  // else {
-  //   res.status(404).json({ status: false, message: "No record found" });
-  // }
 });
