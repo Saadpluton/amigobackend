423fcf68b88ed43b642a65ca71eb0839b6dd2805
Saad.pluton@outlook.com <112068949+Saadpluton@users.noreply.github.com>
2022-11-06 22:18:56 +0500

    updated

--- controllers/UserController/LoginUser.js
+++ controllers/UserController/LoginUser.js
@@ -1,67 +1,70 @@
-import {User , validate} from "#models/UserModel/user"
+import { User, validate } from "#models/UserModel/user";
 import asyncHandler from "#middlewares/asyncHandler";
 import bcrypt from "bcrypt";
 import Joi from "joi";
-import {Artist } from "#models/ArtistModel/artist"
+import { Artist } from "#models/ArtistModel/artist";
 //@desc Login User
 //@route  /user/login
 //@request post Request
 //@acess  public
 
-
-
 export const loginUser = asyncHandler(async (req, res) => {
-    
-    const validate = (req) => {
-        const schema = Joi.object({
-          email: Joi.string().min(5).max(255).required().email(),
-          password: Joi.string().min(5).max(255).required(),
-          role : Joi.string().required()
-        });
-      
-        return schema.validate(req)
-      }
+  const validate = (req) => {
+    const schema = Joi.object({
+      email: Joi.string().min(5).max(255).required().email(),
+      password: Joi.string().min(5).max(255).required(),
+      role: Joi.string().required(),
+    });
 
-      const {error} = validate(req.body);
+    return schema.validate(req);
+  };
 
-      if (error)
-      {
-        return res.status(400).send({status : false , message : error?.details[0]?.message});
-      }
+  const { error } = validate(req.body);
 
-      let userFind , token ;
-     
-      if(req.body.role === 'user')
-      {
-       userFind = await User.findOne({email: req.body?.email });
-       token = userFind?.generateAuthToken();
-      }
-      else if(req.body.role === 'artist')
-      {
-        userFind = await Artist.findOne({email: req.body?.email });
-       token = userFind?.generateAuthToken();
-      }
-      else{
-        return res.status(400).json({ status: false, message: "Role is not valid!" })   
-      }
-    
-    if (!userFind) {
-        return res.status(400).json({ status: false, message: "Email does not exists!" })
-      }
-
-    let valid;
-    if (userFind) {
-        valid = await bcrypt.compare(req.body?.password, userFind?.password)
-    }
-    if (!valid) {
-        return res.status(400).json({ status: false, message: "Email or password incorrect!" })
-    }
-
-
-  if(valid)
-  {
-    return res.status(200)
-    .send({ status: true, message: "login successfully" ,user : userFind ,token})    
+  if (error) {
+    return res
+      .status(400)
+      .send({ status: false, message: error?.details[0]?.message });
   }
 
+  let userFind, token;
+
+  if (req.body.role === "user") {
+    userFind = await User.findOne({ email: req.body?.email });
+    token = userFind?.generateAuthToken();
+  } else if (req.body.role === "artist") {
+    userFind = await Artist.findOne({ email: req.body?.email });
+    token = userFind?.generateAuthToken();
+  } else {
+    return res
+      .status(400)
+      .json({ status: false, message: "Role is not valid!" });
+  }
+
+  if (!userFind) {
+    return res
+      .status(400)
+      .json({ status: false, message: "Email does not exists!" });
+  }
+
+  let valid;
+  if (userFind) {
+    valid = await bcrypt.compare(req.body?.password, userFind?.password);
+  }
+  if (!valid) {
+    return res
+      .status(400)
+      .json({ status: false, message: "Email or password incorrect!" });
+  }
+
+  if (valid) {
+    return res
+      .status(200)
+      .send({
+        status: true,
+        message: "login successfully",
+        user: userFind,
+        token,
+      });
+  }
 });
--- models/SongModel/song.js
+++ models/SongModel/song.js
@@ -2,92 +2,90 @@ import mongoose from "mongoose";
 import Joi from "joi";
 
 const SongSchema = new mongoose.Schema({
-    name: {
-        type: String,
+  name: {
+    type: String,
+  },
+  tag: [
+    {
+      type: String,
     },
-    tag: [{
-        type: String
-    }],
-    description: {
-        type: String
+  ],
+  description: {
+    type: String,
+  },
+  duration: {
+    type: String,
+  },
+  genre: {
+    type: String,
+  },
+  image: {
+    type: String,
+  },
+  artistId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Artist",
+    required: [true, "artistId field is required"],
+  },
+  artistName: {
+    type: String,
+  },
+  year: {
+    type: String,
+  },
+  totalListeners: {
+    type: Number,
+    default: 0,
+  },
+  totalLikes: {
+    type: Number,
+    default: 0,
+  },
+  totalComments: {
+    type: Number,
+    default: 0,
+  },
+  totalShares: {
+    type: Number,
+    default: 0,
+  },
+  audio: {
+    type: String,
+  },
+  subGenre: [
+    {
+      type: String,
     },
-    duration: {
-        type: String,
-    },
-    genre: {
-        type: String
-    },
-    image: {
-        type: String,
-    },
-    artistId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Artist",
-        required: [true, "artistId field is required"]
-    },
-    artistName: {
-        type: String
-    },
-    year: {
-        type: String
-    },
-    totalListeners: {
-        type: Number,
-        default: 0
-    },
-    totalLikes: {
-        type: Number,
-        default: 0
-    },
-    totalComments: {
-        type: Number,
-        default: 0
-    },
-    totalShares: {
-        type: Number,
-        default: 0
-    },
-    audio: {
-        type: String,
-    },
-    subGenre: [{
-        type: String
-    }],
-    isSuspend: {
-        type: Boolean,
-        default: false
-    },
-    isViewed: {
-        type: Boolean,
-        default: 0
-    }
+  ],
+  isSuspend: {
+    type: Boolean,
+    default: false,
+  },
+  isViewed: {
+    type: Boolean,
+    default: 0,
+  },
 });
 
 function validateSong(user) {
-    const schema = Joi.object({
-        name: Joi.string()
-            .required(),
-        tag: Joi.string()
-            .required(),
-        description: Joi.string()
-            .required(),
-        duration: Joi.string(),
-        genre: Joi.string()
-            .required(),
-        //image: Joi.string(),
-        audio: Joi.string(),
-        artistId: Joi.string()
-            .required(),
-        artistName: Joi.string(),
-        year: Joi.string().regex(/^\d{4}\-\d{2}\-\d{2}$/).
-            required(),
-        subGenre: Joi.string()
-    });
-    return schema.validate(user);
+  const schema = Joi.object({
+    name: Joi.string().required(),
+    tag: Joi.string().required(),
+    description: Joi.string().required(),
+    duration: Joi.string(),
+    genre: Joi.string().required(),
+    image: Joi.string(),
+    audio: Joi.string(),
+    artistId: Joi.string().required(),
+    artistName: Joi.string(),
+    year: Joi.string()
+      .regex(/^\d{4}\-\d{2}\-\d{2}$/)
+      .required(),
+    subGenre: Joi.string(),
+  });
+  return schema.validate(user);
 }
 
-const Song = mongoose.model("Song", SongSchema)
+const Song = mongoose.model("Song", SongSchema);
 
-export { Song, validateSong as validate }
-
-
+export { Song, validateSong as validate };
