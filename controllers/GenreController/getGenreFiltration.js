677ad527f72da9f37ae46930b7bdfcfd30fcaf7b
saadpluton1 <saad@pluton.ltd>
2022-11-07 18:31:46 +0500

    updated

--- controllers/GenreController/getGenreFiltration.js
+++ controllers/GenreController/getGenreFiltration.js
@@ -12,14 +12,18 @@ import { Album } from "#models/AlbumModel/album";
 export const getGenresFiltration = asyncHandler(async (req, res) => {
   
   
-  const playlist = await Playlist.find({trackGenre: req.query.genre ,  trackSubGenre: { $in: [req.query.subGenre] } }).select('-__v');
-  const artist = await Artist.find({ genre: { $in: [req.query.genre] } , subGenre: { $in: [req.query.subGenre] }} ).select('-__v');
-  
-  //const album = await Album.find({ genre: req.query.genre , subGenre: req.query.subGenre  }).select('-__v');
+  // const playlist = await Playlist.find({trackGenre: req.query.genre ,  trackSubGenre: { $in: [req.query.subGenre] } }).select('-__v');
+  // const artist = await Artist.find({ genre: { $in: [req.query.genre] } , subGenre: { $in: [req.query.subGenre] }} ).select('-__v');
+  // //const album = await Album.find({ genre: { $in: [req.query.genre] } , subGenre: { $in: [req.query.subGenre] }).select('-__v');
 
-if(playlist.length > 0 || artist.length > 0)
+  const playlist = await Playlist.find({trackGenre: req.query.genre}).select('-__v');
+  const artist = await Artist.find({ genre: { $in: [req.query.genre] }} ).select('-__v');
+  const album = await Album.find({ genre: req.query.genre}).select('-__v');
+  
+  
+if(playlist.length > 0 || artist.length > 0 || album.length > 0 )
   {
-    return res.status(200).json({playlist : playlist , artist : artist});    
+    return res.status(200).json({playlist : playlist , artist : artist , album : album });    
   }
   else{
      return res.status(200).json({status : true , message : "No record found"});
