1fa6270ab6ea9802cffb92e3a1171a36af34ae27
saadpluton1 <saad@pluton.ltd>
2022-11-11 13:24:18 +0500

    updated

--- routes/AdminRoutes/admin_route.js
+++ routes/AdminRoutes/admin_route.js
@@ -5,6 +5,7 @@ import { createTrendingTrack } from "#controllers/AdminController/CreateTrending
 import { getPlaylists } from "#controllers/AdminController/getAllPlaylist";
 import { getTracks } from "#controllers/AdminController/getAllTrack";
 import { getAlbums } from "#controllers/AdminController/getAllAlbum";
+import { getArtist } from "#controllers/AdminController/getAllArtist";
 
 
 const AdminRoute = express.Router();
@@ -15,12 +16,13 @@ AdminRoute.get("/trackTrending/:id",validateObjectId, createTrendingTrack);
 // Admin All Playlist
 AdminRoute.get("/playlistAdmin", getPlaylists);
 
-
 // Admin All Track
 AdminRoute.get("/trackAdmin", getTracks);
 
-
 // Admin All Albums
 AdminRoute.get("/albumAdmin", getAlbums);
+
+// Admin All Artist
+AdminRoute.get("/artistAdmin", getArtist);
 
 export default AdminRoute;
